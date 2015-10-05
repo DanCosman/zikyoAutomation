4db8fb44f41a9718b16956ed30cb9960500b2fe7
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2015-10-05 22:28:04 +0300

    added screenshots functionality in hooks

--- src/test/java/com/zikyo/steps/Hooks.java
+++ src/test/java/com/zikyo/steps/Hooks.java
@@ -1,10 +1,13 @@
 package com.zikyo.steps;
 
+import com.sdl.selenium.utils.config.WebDriverConfig;
 import com.zikyo.common.InitDriver;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import java.io.IOException;
 
@@ -19,6 +22,12 @@ public class Hooks {
 
     @After
     public void tearDown(Scenario scenario) {
+        String scenarioName = scenario.getName();
+        if (scenario.isFailed()) {
+            LOGGER.warn("\"" + scenarioName + "\" scenario has failed! Screenshot embedded in the report.");
+            byte[] screenshot = ((TakesScreenshot) WebDriverConfig.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png");
+        }
         LOGGER.debug("***** END of Scenario: " + scenario.getName() + " *****");
     }
 }
