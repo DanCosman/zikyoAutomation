9659c9c0c8ba722e147d2c7fdf598d2392c10974
dcosman <cosmandaniel@gmail.com>
2015-11-05 12:16:04 +0200

    zikyo

--- src/test/java/com/zikyo/steps/genericSteps/GenericSteps.java
+++ src/test/java/com/zikyo/steps/genericSteps/GenericSteps.java
@@ -6,6 +6,7 @@ import com.sdl.selenium.utils.config.WebDriverConfig;
 import com.sdl.selenium.web.SearchType;
 import com.sdl.selenium.web.WebLocator;
 import com.sdl.selenium.web.WebLocatorDriverExecutor;
+import com.sdl.selenium.web.button.Button;
 import com.sdl.selenium.web.button.InputButton;
 import com.sdl.selenium.web.form.ComboBox;
 import com.sdl.selenium.web.form.TextField;
@@ -14,6 +15,7 @@ import com.sdl.selenium.web.table.TableRow;
 import com.thoughtworks.selenium.webdriven.ElementFinder;
 import com.zikyo.steps.TestBase;
 import com.zikyo.utils.TestUtils;
+import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -25,6 +27,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.swing.*;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -50,6 +53,24 @@ public class GenericSteps extends TestBase {
         link.assertClick();
     }
 
+    //public static void main(String[] args) {
+        //WebLink link = new WebLink().setText("Adviser", SearchType.TRIM, SearchType.EQUALS, SearchType.CHILD_NODE);
+        //System.out.println(link.getXPath());
+   // }
+
+
+    @When("^I click on link with text2 \"([^\"]*)\"$")
+    public void I_click_on_link_with_text2(String text) {
+        WebLink link = new WebLink().setText(text, SearchType.TRIM, SearchType.EQUALS, SearchType.CHILD_NODE);
+        link.assertClick();
+    }
+
+    @When("^I click on link with text3 \"([^\"]*)\"$")
+    public void I_click_on_link_with_Text3(String text) {
+        WebLink link = new WebLink().setText(text, SearchType.TRIM, SearchType.EQUALS, SearchType.DEEP_CHILD_NODE);
+        link.assertClick();
+    }
+
     @When("^I click on element with text \"([^\"]*)\"$")
     public void I_click_on_element_with_text(String text) throws Throwable {
         WebLocator element = new WebLocator().setText(text);
@@ -57,15 +78,31 @@ public class GenericSteps extends TestBase {
     }
 
     @When("^I click on \"([^\"]*)\" button from the section \"([^\"]*)\"$")
-    public void I_click_on_element_from_container_with_text(String button, String section) {
+    public void I_click_on_element_from_container_with_text(String text, String section) {
         Form form = new Form().setTitle(section);
-        WebLink link = new WebLink(form).setText(button, SearchType.DEEP_CHILD_NODE_OR_SELF);
+        WebLink link = new WebLink(form).setText(text, SearchType.DEEP_CHILD_NODE_OR_SELF);
         link.assertClick();
     }
+
+    @When("^I click on \"([^\"]*)\" on visible \"([^\"]*)\" form$")
+    public void clickOnButton(String text, String legend) {
+        WebLocator visibleWrapper = new WebLocator().setClasses("wrapper").setExcludeClasses("hide");
+        Form form = new Form(visibleWrapper).setTitle(legend);
+        WebLink link = new WebLink(form).setText(text, SearchType.DEEP_CHILD_NODE_OR_SELF);
+        link.assertClick();
+    }
+
 
     @When("^I click on input button with text \"([^\"]*)\"$")
     public void I_click_on_input_button_with_text(String text) {
         InputButton button = new InputButton().setText(text);
+        button.assertClick();
+    }
+
+
+    @When("^I click on  button2 with text \"([^\"]*)\"$")
+    public void I_click_on_button2_with_text(String text) {
+        Button button = new Button().setText("Login", SearchType.DEEP_CHILD_NODE_OR_SELF).setVisibility(true);
         button.assertClick();
     }
 
@@ -88,6 +125,12 @@ public class GenericSteps extends TestBase {
         assertThat("Failed to set value in field.", field.setValue(value));
     }
 
+    @When("^I type \"([^\"]*)\" into2 \"([^\"]*)\" field$")
+    public void typeIntoField_2(String value, String label) {
+        TextField field = new TextField().setLabel(label).setLabelPosition("//following-sibling::").setVisibility(true);
+        assertThat("Failed to set value in field.", field.setValue(value));
+    }
+
 
     @Then("^field \"([^\"]*)\" should have value \"([^\"]*)\"$")
     public void text_field_with_label_should_have_value(String label, String value) {
@@ -103,7 +146,7 @@ public class GenericSteps extends TestBase {
 
     @Given("^I set browser size to (\\d+), (\\d+)$")
     public void setBrowserSize(int width, int height) {
-       LOGGER.info("I set browser size to ({}, {})", width, height);
+        LOGGER.info("I set browser size to ({}, {})", width, height);
         WebDriverConfig.getDriver().manage().window().setSize(new Dimension(width, height));
     }
 
@@ -130,7 +173,7 @@ public class GenericSteps extends TestBase {
     }
 
     @And("^I select \"(.+?)\" in the section \"(.+?)\" from the \"(.+?)\" form$")
-    public void selectRadioFromSection(String value, String section, String formTitle){
+    public void selectRadioFromSection(String value, String section, String formTitle) {
         Form parentForm = new Form().setTitle(formTitle);
         WebLocator parentDiv = new WebLocator(parentForm).setLabel(section).setLabelPosition("//following::").setTag("div");
         WebLocator radio = new WebLocator().setLabel(value).setLabelPosition("//preceding-sibling::").setTag("input").setType("radio");
@@ -138,23 +181,77 @@ public class GenericSteps extends TestBase {
         clickableSpan.assertClick();
     }
 
-    private WebLocator addVisibilityCheck(WebLocator locator) {
-        return locator.setElPathSuffix("visibility", "count(ancestor::*[contains(concat(' ', @class, ' '), ' hide ')])=0");
+    //private WebLocator addVisibilityCheck(WebLocator locator) {
+    //return locator.setElPathSuffix("visibility", "count(ancestor::*[contains(concat(' ', @class, ' '), ' hide ')])=0");
+
+
+    public static void main (String[] args) {
+        Button button = new Button().setText("Login",SearchType.DEEP_CHILD_NODE_OR_SELF);
+        System.out.println(button.getXPath());
     }
 
-    public static void main(String[] args) {
-        Form parentForm = new Form().setTitle("Drivers");
-        WebLocator parentDiv = new WebLocator(parentForm).setLabel("Are you married?").setLabelPosition("//following::");
-        WebLocator radio = new WebLocator(parentDiv).setLabel("Yes").setLabelPosition("//preceding-sibling::").setTag("input").setType("radio");
-        System.out.println(radio.getXPath());
-    }
+    // public static void I_click_on_element_by_class(String[] args) {
+    //   WebLink link = new WebLink().setText("Continue", SearchType.CHILD_NODE);
+    // System.out.println(link.getXPath());
+
+
+    // public static I_click_on_GenericSteps(String[] args) {
+    //InputButton button = new InputButton().setText("Login", SearchType.DEEP_CHILD_NODE_OR_SELF);
+    // System.out.println(button.getXPath());
+
+
+//        Form parentForm = new Form().setTitle("Drivers");
+
+//        WebLocator subTitle = new WebLocator().setText("Add Driver Primary Applicant");
+//        Form parentForm = new Form().setTitle("Drivers").setChildNodes(subTitle);
+
+//        WebLocator visibleContainer = new WebLocator().setCls("wrapper demo");
+//        Form parentForm = new Form(visibleContainer).setTitle("Drivers");
+
+//        WebLocator visibleContainer = new WebLocator().setClasses("wrapper").setExcludeClasses("hide");
+//        Form parentForm = new Form(visibleContainer).setTitle("Drivers");
+
+    // WebLocator visibleContainer = new WebLocator().setClasses("wrapper").setExcludeClasses("hide");
+    // Form parentForm = new Form(visibleContainer).setTitle("Drivers");
+
+    //WebLink link = new WebLink(parentForm).setText("Continue", SearchType.DEEP_CHILD_NODE_OR_SELF);
+    //System.out.println(link.getXPath());
+
 
     @And("^I select \"([^\"]*)\" in the active drop-down list \"([^\"]*)\"$")
     public void selectValueInActiveDropdown(String value, String label) {
-        WebLocator parentDiv = new WebLocator().setClasses("active","custom-select");
+        WebLocator parentDiv = new WebLocator().setClasses("active", "custom-select");
         ComboBox comboBox = new ComboBox(parentDiv).setLabel(label).setLabelPosition("//following::");
         assertThat("Failed to select " + value, comboBox.select(value));
 
     }
+
+    @And("^I click on label \"([^\"]*)\"$")
+    public void I_click_on_label(String text) throws Throwable {
+        WebLocator label = new WebLocator().setText(text).setTag("label");
+        label.assertClick();
+
+    }
+
+    @And("^I click on button with text \"([^\"]*)\"$")
+    public void I_click_on_button_with_text(String text) throws Throwable {
+        Button button = new Button().setText(text,SearchType.DEEP_CHILD_NODE_OR_SELF).setVisibility(true);
+        button.assertClick();
+    }
+
+    @And("^I type \"([^\"]*)\" into \"([^\"]*)\" field with input type$")
+    public void I_type_into_field_with_input_type(String value, String label) throws Throwable {
+        TextField field = new TextField().setLabel(label).setLabelPosition("//following::");
+        assertThat("Failed to set value in field.", field.setValue(value));
+    }
+
+
+
+    @And("^I select \"([^\"]*)\" in the drop-down list \"([^\"]*)\"$")
+    public void I_select_in_the_drop_down_list(String value, String label) throws Throwable {
+        ComboBox comboBox = new ComboBox().setLabel(label).setLabel(label,SearchType.DEEP_CHILD_NODE_OR_SELF).setVisibility(true);
+        assertThat("Failed to select " + value, comboBox.select(value));
+    }
+
 
 }
--- src/test/resources/features/validation-test.feature
+++ src/test/resources/features/validation-test.feature
@@ -1,37 +1,48 @@
 Feature: Validation test
 
-  Scenario: Add your Primary Vehicle 2012 Audi A4
-    Given I open "http://auto.savvyinsured.com/forms/"
-    And I wait 1 seconds
-    When I select "2000" in the drop-down list named "Vehicle Year"
-    And I select "Audi" in the drop-down list named "Vehicle Make"
-    And I select "A4" in the drop-down list named "Vehicle Model"
+  Scenario: Nowpensions
+   Given I open "http://staging.nowpensions.com/"
+   #And I click on link with text "Login"
+   And I click on link with text2 "Adviser"
+   And I wait 2 seconds
+   And I click on link with text3 "Login"
+    And I wait 2 seconds
+    And I type "testare1984+9999@gmail.com" into2 "Email" field
+    And I type "12345678" into2 "Password" field
+   And I click on  button2 with text "Login"
+   And I wait 2 seconds
+   And I click on link with text3 "Start a new application"
+   And I click on link with text3 "Continue"
+   And I click on element with text "Yes"
+   And I click on label "My bureau is not shown"
+   And I click on input button with text "Continue"
+   And I click on button with text "Choose plan"
+   And I wait 2 seconds
+   And I type "asaasa" into "Employer name (legal)" field with input type
+   And I click on element with text "Employer name (known as)"
+    And I wait 2 seconds
+    And I click on button with text "Yes"
+    And I select "1st May 2016" in the drop-down list with label in separate div "What is your initial auto enrolment staging date?"
+    #And I click on input button with text "No"
+    And I select "Public Limited Company" in the drop-down list with label in separate div "What describes your organisation most accurately?"
+    And I click on element with text "If there is no company/charity registration no. please tick this box"
+    And I type "Str.Mihai Viteazu nr.40" into "Company registered address" field with input type
+    And I type "Cluj-Napoca" into "City" field with input type
+    And I type "0989798787" into "Postcode" field with input type
+    And I select "21-30" in the drop-down list with label in separate div "How many employees do you want to include in the NOW: Pensions Plan?"
+    And I type "asdafdsa" into "Primary contact name at company for queries about the Plan" field with input type
+    And I type "testare1984+9999@gmail.com" into "Primary contact email address" field with input type
+    And I type "testare1984+9999@gmail.com" into "Primary contact email address (confirm)" field with input type
+    And I type "Boss" into "Primary contact job title" field with input type
+    And I type "0986735836345683" into "Primary contact phone number" field with input type
+    And I type "045/AB765" into "What is the PAYE Reference of your largest payroll? (if you only have one Payroll, please use that number)" field with input type
+    And I type "Ionut" into "Main payroll contact name" field with input type
+    And I type "testare1984+9999@gmail.com" into "Main payroll contact email" field with input type
+    And I type "testare1984+9999@gmail.com" into "Main payroll contact email (confirm)" field with input type
+    And I type "0987832434" into "Main payroll contact telephone" field with input type
+    #And I type "asdfsda" into "What is the payroll commonly known as?" field with input
+    And I wait 2 seconds
     And I click on element with text "Continue"
-    And I wait 2 seconds
-    And I click on "Continue" button from the section "Your Vehicles"
-    And I wait 1 seconds
-    And I type "john" into "Your First Name" field
-    And I type "McDonald" into "Your Last Name" field
-    And I type "10/10/84" into "mm/dd/yyyy" field
-    And I click on element with text "Male"
-    And I click on element with text "Female"
-    #And I click on element with text "Back"
-    #And I click on element with text "Add another vehicle"
-    #And I wait 2 seconds
-    #And I select "2002" in the active drop-down list "Vehicle Year"
-    #And I select "BMW" in the drop-down list named "Select Make"
-    #And I select "7 Series" in the drop-down list named "Vehicle Model"
-    And I wait 1 seconds
-    And I click on "Continue" button from the section "Drivers"
-    And I wait 2 seconds
-    And I select "High School Diploma" in the drop-down list with label in separate div "Education"
-    And I select "Lawyer" in the drop-down list with label in separate div "Occupation"
-    And I select "Excellent" in the drop-down list with label in separate div "Credit Rating"
-    And I select "31" in the drop-down list with label in separate div "Age You Got Your First US Driver's License?"
-    And I wait 2 seconds
-    And I select "No" in the section "Are you married?" from the "Drivers" form
-    And I select "No" in the section "Is Your License Active?" from the "Drivers" form
-    And I select "Yes" in the section "Do you require SR-22/SR-1P filing?" from the "Drivers" form
-    And I select "Yes" in the section "Tickets or claims in the last 3 years?" from the "Drivers" form
-    And I wait 2 seconds
-
+    #And I select "Fortnightly" in the drop-down list "Payroll Frequency"
+   And I wait 10 seconds
+   
